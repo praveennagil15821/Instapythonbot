5b321bc2e750612131300226f5146d1f9d346d0a
praveennagil <praveennagil15821@gmail.com>
2020-04-26 10:18:30 -0700

    added followers and following extraction

--- database.py
+++ database.py
@@ -1,41 +1,22 @@
-import os,csv
+import os
 import pandas as pd
 from utility_methods.utility_methods import *
 
 base_dir=check_dir('./database/')
 
-class super:
-    
-    def add(self,username,acctype='None'):      
-        read=pd.read_csv(self.file)
-        if type(username) is  type(list()) and (acctype=='None' or type(acctype) is type(list())):
-            if acctype=='None':
-                for x in username:
-                    self.add(x)  
-            else:
-                for x,y in zip(username,acctype):
-                    self.add(x,y) 
-        else:             
-            if (username in read.user.values):            
-                read.loc[read.user == username,'priority']=int(read.loc[read.user == username].priority.values[0]+1)
-                read.loc[read.user == username,'acctype']=acctype
-                read.set_index('user', inplace=True) 
-                read.to_csv(self.file,mode='w',index=True, header=True)
-            else:
-                data={
-                    'user':[username],
-                    'acctype':[acctype],                        
-                    'priority':[1]
-                }
-                read=pd.DataFrame(data)
-                read.set_index('user', inplace=True)            
-                read.to_csv(self.file,mode='a',index=True, header=False)
-            return None    
+class super:    
 
     def get_users(self):
         read=pd.read_csv(self.file)
+        read.set_index('user',inplace=True)
+        df=read.groupby(['user']).size()
+        read=pd.DataFrame(df)
+        read.rename(columns={0:'priority'},inplace=True)
         read.sort_values(['priority'],ascending=False, inplace=True)
-        return list(read.user.values)
+        read.to_csv(self.file,mode='w',index=True, header=True)        
+        new=pd.read_csv(self.file)
+        #print(new.user.tolist())
+        return list(new.user.values)
 
     def remove(self,username):
         read=pd.read_csv(self.file) 
@@ -43,54 +24,93 @@ class super:
         #print(read.index.values)    
         #print('droped',read.drop(username))
         if (username in read.index.values):
-            pass
-            #
             read.drop(username,inplace=True)
             #read.set_index('user', inplace=True) 
             read.to_csv(self.file,mode='w',index=True, header=True)
         else:
-            print('notfound')    
-        
-    
+            return False
 
-class TargetUsers(super):
+    def add(self,users):
+
+        if type(users) is type(list()):
+            data={
+                'user':users,
+            }           
+            
+        else:
+            data={
+                'user':[users],
+            }
+        read=pd.DataFrame(data)
+        read.set_index('user', inplace=True)            
+        read.to_csv(self.file,mode='a',index=True, header=False)
+
+
+    
+        
+
+class TargetList(super):
 
     def __init__(self):
-        self.file=base_dir+'target_users'+".csv"
-        self.columns = ['user', 'acctype', 'priority']      
+        self.file=base_dir+'target_list'+".csv"
+        self.columns = ['user']     
         self.df=pd.DataFrame(columns=self.columns)  
         self.df.set_index('user', inplace=True)          
         self.df.to_csv(self.file, mode='a',index=True, header=(not os.path.exists(self.file)))
         
+
+class HitList(super):
+
+    def __init__(self):
+        self.file=base_dir+'hit_list'+".csv"
+        self.columns = ['user']     
+        self.df=pd.DataFrame(columns=self.columns)  
+        self.df.set_index('user', inplace=True)          
+        self.df.to_csv(self.file, mode='a',index=True, header=(not os.path.exists(self.file)))
+
+class FameList(super):
     
+    def __init__(self):
+        self.file=base_dir+'fame_list'+".csv"
+        self.columns = ['user']     
+        self.df=pd.DataFrame(columns=self.columns)  
+        self.df.set_index('user', inplace=True)          
+        self.df.to_csv(self.file, mode='a',index=True, header=(not os.path.exists(self.file)))  
         
-        
-
-
+class PrivateList(super):
+    
+    def __init__(self):
+        self.file=base_dir+'private_list'+".csv"
+        self.columns = ['user']     
+        self.df=pd.DataFrame(columns=self.columns)  
+        self.df.set_index('user', inplace=True)          
+        self.df.to_csv(self.file, mode='a',index=True, header=(not os.path.exists(self.file)))            
+ 
 class ContentLabours(super):
     def __init__(self):
         self.file=base_dir+'content_labours'+".csv"
-        self.columns = ['user', 'acctype', 'priority']      
+        self.columns = ['user']     
         self.df=pd.DataFrame(columns=self.columns)  
         self.df.set_index('user', inplace=True)          
         self.df.to_csv(self.file, mode='a',index=True, header=(not os.path.exists(self.file)))
 
-    
-
+ 
    
         
 if __name__ == "__main__":
     print('ok')
-    a=ContentLabours()
-    a.add('pankaj_nagil','Public')
-    ContentLabours().remove('pankaj_nagil')
+    #a=TargetList()
+    #a.add(['kholke_to_dekho'])
+    #print(a.get_users())
+    # a.add('pankaj_nagil','Public')
+    # ContentLabours().remove('pankaj_nagil')
     
-    #print(TargetUsers().get_users())
-    TargetUsers().add(['pankaj_nagil','khaoll'],['public','private'])
-    #TargetUsers().remove(['stoned_bhaiya','pankaj_l'])
-    # a.add('stoned_bhaiya','Public')
-    b=TargetUsers()
-    b.add('pankaj_l','Public')
-    b.add('stoned_a','Private')
+    # #print(TargetUsers().get_users())
+    # TargetUsers().add(['pankaj_nagil','khaoll'],['public','private'])
+    # #TargetUsers().remove(['stoned_bhaiya','pankaj_l'])
+    # # a.add('stoned_bhaiya','Public')
+    # b=TargetUsers()
+    # b.add('pankaj_l','Public')
+    # b.add('stoned_a','Private')
 
 
--- instagram.py
+++ instagram.py
@@ -1,14 +1,13 @@
 from selenium import webdriver
-#from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 import time
 from utility_methods.utility_methods import *
-from content.contentcollector import collector,contentadd
 import urllib.request
 import os
-import notification
+import notification,database
 
 
 class InstaBot():
@@ -21,8 +20,11 @@ class InstaBot():
         self.login_url = config['IG_URLS']['LOGIN']
         self.nav_user_url = config['IG_URLS']['NAV_USER']
         self.get_tag_url = config['IG_URLS']['SEARCH_TAGS']
+        self.mainuser='stoned_bhaiya'
 
         self.driver = webdriver.Chrome()
+        self.wait = WebDriverWait(self.driver, 15)
+        self.count = 0
 
     @insta_method
     def login(self):
@@ -32,9 +34,9 @@ class InstaBot():
 
         self.driver.get(self.login_url)
         
-        WebDriverWait(self.driver, 5).until(EC.presence_of_element_located((By.NAME,'username')))
+        self.wait.until(EC.presence_of_element_located((By.NAME,'username')))
         username_input = self.driver.find_element_by_name('username')
-        WebDriverWait(self.driver, 5).until(EC.presence_of_element_located((By.NAME,'password')))        
+        self.wait.until(EC.presence_of_element_located((By.NAME,'password')))        
         password_input = self.driver.find_element_by_name('password')
                 
                 
@@ -48,7 +50,7 @@ class InstaBot():
         # login_btn = self.driver.find_element_by_xpath(
             # '//button//div[text() = "Log In"]')
 
-        WebDriverWait(self.driver, 5).until(EC.presence_of_element_located((By.XPATH, r'//button//div[text() = "Log In"]')))
+        self.wait.until(EC.presence_of_element_located((By.XPATH, r'//button//div[text() = "Log In"]')))
         login_btn= self.driver.find_element_by_xpath(r'//button//div[text() = "Log In"]')
 
         login_btn.click()
@@ -70,6 +72,114 @@ class InstaBot():
         print("System down -- Successfull")
         notification.msg('Alert!','System down -- Successfull')
 
+    def grabfollow(self):
+        
+        self.count=0
+        def check_difference_in_count(driver):
+            new_count = len(driver.find_elements_by_xpath("//div[@role='dialog']//li"))
+            if self.count != new_count:
+                self.count = new_count
+                #print('till now follow count-',new_count)
+                return True
+            else:
+                #print('Final follow count-',new_count)
+                return False
+        
+        while True:
+        # scroll down            
+            time.sleep(2)
+            WebDriverWait(self.driver, 30).until(EC.presence_of_all_elements_located((By.CSS_SELECTOR,"div[role=dialog] ul div")))
+            x = self.driver.find_elements_by_css_selector(r'div[role=dialog] ul div')
+            self.driver.execute_script("arguments[0].scrollIntoView();",x[-1])
+           
+            try:
+                WebDriverWait(self.driver, 5).until(check_difference_in_count)                
+            except:
+                break
+        
+        l=self.driver.find_elements_by_xpath("//div[@role='dialog']//li//img")
+        user=[]
+        print(len(l))
+        for i in l:
+            temp=str(i.get_attribute("alt"))
+            user.append(temp.split("'s")[0])
+        return user
+        #print([x.get_attribute("alt") for x in l])    
+    
+        
+    def account_details(self,username):
+        self.driver.get(self.nav_user_url.format(username))
+        
+        #try:
+        #posts = self.wait.until(EC.presence_of_element_located((By.XPATH, "//li/span[text()=' posts')]/span"))).text
+        # except:
+        #     posts = self.wait.until(EC.presence_of_element_located((By.XPATH, '//li/span[text()=" post"]/span'))).text
+        try:
+            followers = str(self.wait.until(EC.presence_of_element_located((By.XPATH, '//li/a[text()=" followers"]/span'))).text).replace(',','')
+            following = str(self.wait.until(EC.presence_of_element_located((By.XPATH, '//li/a[text()=" following"]/span'))).text).replace(',','')
+            acctype='Public'
+            if followers.islower() or following.islower():
+                if followers.islower():
+                    followers = str(self.driver.find_element_by_xpath('//li/a[text()=" followers"]/span').get_attribute('title'))
+                    followers=followers.replace(',','')
+                if following.islower():
+                    following = str(self.driver.find_element_by_xpath('//li/a[text()=" following"]/span').get_attribute('title'))
+                    following=following.replace(',','')
+        except:
+            followers = str(self.wait.until(EC.presence_of_element_located((By.XPATH, '//li/span[text()=" followers"]/span'))).text).replace(',','')
+            following = str(self.wait.until(EC.presence_of_element_located((By.XPATH, '//li/span[text()=" following"]/span'))).text).replace(',','')
+            acctype='Private'
+            if followers.islower() or following.islower():
+                if followers.islower():
+                    followers = str(self.wait.until(EC.presence_of_element_located((By.XPATH, '//li/span[text()=" followers"]/span'))).get_attribute('title'))
+                    followers=followers.replace(',','')
+                if following.islower():
+                    following = str(self.wait.until(EC.presence_of_element_located((By.XPATH, '//li/span[text()=" following"]/span'))).get_attribute('title'))
+                    following=following.replace(',','')
+       
+        ratio=int(following)/int(followers)
+        print(' followers-',followers," following-",following,"ratio",ratio)
+        if acctype == 'Public':
+            if int(followers)>0:
+                self.driver.find_element_by_partial_link_text("followers").click()
+                followers_list=self.grabfollow()
+                self.driver.refresh()
+            else:followers_list=[]    
+            if int(following)>0:
+                self.driver.find_element_by_partial_link_text("following").click()
+                following_list=self.grabfollow()
+                self.driver.refresh()
+            else:following_list=[]    
+            return {'followers':followers_list,'following':following_list,'ratio':ratio}
+        else:            
+            return acctype
+
+    def hit(self):
+        target=database.TargetList().get_users()
+        followers=self.account_details(self.mainuser)['followers']
+        for i in target[:3]:
+            if i in followers:
+                database.HitList().add(i)
+                database.TargetList().remove(i)
+                continue
+            acc=self.account_details(i)
+            if acc=='Private':
+                print('acc is private')
+                database.PrivateList().add(i)
+                database.TargetList().remove(i)
+
+            else:
+                users=acc['followers']
+                users.extend(acc['following'])
+                database.TargetList().add(users)
+                database.HitList().add(i)
+                database.TargetList().remove(i)
+
+             
+        
+            
+
+
 
 if __name__ == '__main__':
 
@@ -77,9 +187,11 @@ if __name__ == '__main__':
     logger_file_path = './bot.log'
     config = init_config(config_file_path)
     logger = get_logger(logger_file_path)
-    
-    # bot=InstaBot()
-    # bot.login()
+    bot=InstaBot()
+    #print(bot.absolute('36.2m'))
+    bot.login()
+    #database.TargetList().add('pankaj_nagil')
+    bot.hit()
     
     #bot.quit()
     #bot.like_latest_posts('johngfisher', 2, like=True)
